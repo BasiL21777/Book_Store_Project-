9528eaae39e340cf740419dd8513b72a8da1594e
Basil yaser <basel.yaser205@gmail.com>
2024-05-17 21:36:54 +0300

    Upload the Database DDL

---
+++ MYSQL/DDL.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE book_store1;
+
+USE book_store1;
+
+CREATE TABLE `categories` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_name` (`name`)
+);
+
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `role` varchar(255) NOT NULL,
+    `avatar` varchar(255) DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_email` (`email`)
+);
+
+CREATE TABLE `books` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `author` varchar(255) NOT NULL,
+    `description` text DEFAULT NULL,
+    `price` decimal(10, 2) NOT NULL,
+    `cover_image` varchar(300) DEFAULT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `category_id` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_category_id` (`category_id`),
+    CONSTRAINT `fk_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `cart` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `book_id` int(11) NOT NULL,
+    `quantity` int(11) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `user_id` (`user_id`),
+    KEY `book_id` (`book_id`),
+    CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
+);
